10a5fc5027e79e3f5cc96abd7f4f2d05fb31c1e6
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-09 07:50:31 +0200

    Used new refactored logic of CovRcov

--- src/main/java/com/godaddy/sonar/ruby/simplecovrcov/SimpleCovRcovJsonParserImpl.java
+++ src/main/java/com/godaddy/sonar/ruby/simplecovrcov/SimpleCovRcovJsonParserImpl.java
@@ -27,51 +27,49 @@ public class SimpleCovRcovJsonParserImpl implements SimpleCovRcovJsonParser {
     private static final Logger LOG = Loggers.get(SimpleCovRcovJsonParserImpl.class);
 
     public Map<String, CoverageMeasuresBuilder> parse(File file) throws IOException {
-        Map<String, CoverageMeasuresBuilder> coveredFiles = Maps.newHashMap();
-
         CoverageReport coverageReport = readAndParseReportFile(file);
+        return processCoverageReport(coverageReport);
 
-        String fileString = FileUtils.readFileToString(file, "UTF-8");
-
-        JsonParser parser = new JsonParser();
-        JsonObject resultJsonObject = parser.parse(fileString).getAsJsonObject();
-
-        String coverageRootNode = "";
-        if(resultJsonObject.get("RSpec") != null) { coverageRootNode = "RSpec"; }
-        if(resultJsonObject.get("MiniTest") != null) { coverageRootNode = "MiniTest"; }
-
-        if(coverageRootNode.equals("")) { return coveredFiles; }
-
-        JsonObject coverageJsonObj = resultJsonObject.get(coverageRootNode).getAsJsonObject().get("coverage").getAsJsonObject();
-
-        // for each file in the coverage report
-        for (int j = 0; j < coverageJsonObj.entrySet().size(); j++) {
-            CoverageMeasuresBuilder fileCoverage = CoverageMeasuresBuilder.create();
-
-            String filePath = ((Map.Entry) coverageJsonObj.entrySet().toArray()[j]).getKey().toString();
-            LOG.debug("filePath " + filePath);
-
-            JsonArray coverageArray = coverageJsonObj.get(filePath).getAsJsonArray();
-
-            // for each line in the coverage array
-            for (int i = 0; i < coverageArray.size(); i++) {
-                Long line = null;
-
-                if (!coverageArray.get(i).isJsonNull()) {
-                    line = coverageArray.get(i).getAsLong();
-                }
-
-                Integer intLine = 0;
-                int lineNumber = i + 1;
-                if (line != null) {
-                    intLine = line.intValue();
-                    fileCoverage.setHits(lineNumber, intLine);
-                }
-            }
-            LOG.info("FILE COVERAGE = " + fileCoverage.getCoveredLines());
-            coveredFiles.put(filePath, fileCoverage);
-        }
-        return coveredFiles;
+//        String fileString = FileUtils.readFileToString(file, "UTF-8");
+//
+//        JsonParser parser = new JsonParser();
+//        JsonObject resultJsonObject = parser.parse(fileString).getAsJsonObject();
+//
+//        String coverageRootNode = "";
+//        if(resultJsonObject.get("RSpec") != null) { coverageRootNode = "RSpec"; }
+//        if(resultJsonObject.get("MiniTest") != null) { coverageRootNode = "MiniTest"; }
+//
+//        if(coverageRootNode.equals("")) { return coveredFiles; }
+//
+//        JsonObject coverageJsonObj = resultJsonObject.get(coverageRootNode).getAsJsonObject().get("coverage").getAsJsonObject();
+//
+//        // for each file in the coverage report
+//        for (int j = 0; j < coverageJsonObj.entrySet().size(); j++) {
+//            CoverageMeasuresBuilder fileCoverage = CoverageMeasuresBuilder.create();
+//
+//            String filePath = ((Map.Entry) coverageJsonObj.entrySet().toArray()[j]).getKey().toString();
+//            LOG.debug("filePath " + filePath);
+//
+//            JsonArray coverageArray = coverageJsonObj.get(filePath).getAsJsonArray();
+//
+//            // for each line in the coverage array
+//            for (int i = 0; i < coverageArray.size(); i++) {
+//                Long line = null;
+//
+//                if (!coverageArray.get(i).isJsonNull()) {
+//                    line = coverageArray.get(i).getAsLong();
+//                }
+//
+//                Integer intLine = 0;
+//                int lineNumber = i + 1;
+//                if (line != null) {
+//                    intLine = line.intValue();
+//                    fileCoverage.setHits(lineNumber, intLine);
+//                }
+//            }
+//            LOG.info("FILE COVERAGE = " + fileCoverage.getCoveredLines());
+//            coveredFiles.put(filePath, fileCoverage);
+//        }
     }
 
     private CoverageReport readAndParseReportFile(File file) throws IOException {
@@ -86,6 +84,13 @@ public class SimpleCovRcovJsonParserImpl implements SimpleCovRcovJsonParser {
             coverageReport.addReporter(buildReporter(coverageMapEntry));
         }
         return coverageReport;
+    }
+
+    private Map<String, CoverageMeasuresBuilder> processCoverageReport(CoverageReport coverageReport) {
+        if (coverageReport.hasSeveralReporters()) {
+            LOG.warn("Coverage report has several reporters! Notice, please, that only the first one will be considered by Sonar!");
+        }
+        return processReporter(coverageReport.getFirstReporter());
     }
 
     private Reporter buildReporter(Map.Entry coverageMapEntry) {
@@ -106,5 +111,27 @@ public class SimpleCovRcovJsonParserImpl implements SimpleCovRcovJsonParser {
             marks.add(new Mark(marksEl.toString(), marksEl.isJsonNull()));
         }
         return new ReporterItem(filename, marks);
+    }
+
+    private Map<String, CoverageMeasuresBuilder> processReporter(Reporter reporter) {
+        Map<String, CoverageMeasuresBuilder> coveredFiles = Maps.newHashMap();
+        for (ReporterItem reporterItem : reporter.getItems()) {
+            CoverageMeasuresBuilder fileCoverage = processReporterItem(reporterItem);
+            coveredFiles.put(reporterItem.getFilename(), fileCoverage);
+        }
+        return coveredFiles;
+    }
+
+    private CoverageMeasuresBuilder processReporterItem(ReporterItem reporterItem) {
+        CoverageMeasuresBuilder fileCoverage = CoverageMeasuresBuilder.create();
+        for (int markId = 0; markId < reporterItem.getMarks().size(); markId++) {
+            Mark mark = (Mark)reporterItem.getMarks().toArray()[markId];
+            if (!mark.getIsNull()) {
+                int intLine = mark.getAsLong().intValue();
+                int lineNumber = markId + 1;
+                fileCoverage.setHits(lineNumber, intLine);
+            }
+        }
+        return fileCoverage;
     }
 }
