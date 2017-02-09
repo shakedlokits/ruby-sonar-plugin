1c773dedde04aa956da83fb4cd0130e17fcdf97a
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-09 07:54:26 +0200

    Finally old logic is fully eliminated

--- src/main/java/com/godaddy/sonar/ruby/simplecovrcov/SimpleCovRcovJsonParserImpl.java
+++ src/main/java/com/godaddy/sonar/ruby/simplecovrcov/SimpleCovRcovJsonParserImpl.java
@@ -29,47 +29,6 @@ public class SimpleCovRcovJsonParserImpl implements SimpleCovRcovJsonParser {
     public Map<String, CoverageMeasuresBuilder> parse(File file) throws IOException {
         CoverageReport coverageReport = readAndParseReportFile(file);
         return processCoverageReport(coverageReport);
-
-//        String fileString = FileUtils.readFileToString(file, "UTF-8");
-//
-//        JsonParser parser = new JsonParser();
-//        JsonObject resultJsonObject = parser.parse(fileString).getAsJsonObject();
-//
-//        String coverageRootNode = "";
-//        if(resultJsonObject.get("RSpec") != null) { coverageRootNode = "RSpec"; }
-//        if(resultJsonObject.get("MiniTest") != null) { coverageRootNode = "MiniTest"; }
-//
-//        if(coverageRootNode.equals("")) { return coveredFiles; }
-//
-//        JsonObject coverageJsonObj = resultJsonObject.get(coverageRootNode).getAsJsonObject().get("coverage").getAsJsonObject();
-//
-//        // for each file in the coverage report
-//        for (int j = 0; j < coverageJsonObj.entrySet().size(); j++) {
-//            CoverageMeasuresBuilder fileCoverage = CoverageMeasuresBuilder.create();
-//
-//            String filePath = ((Map.Entry) coverageJsonObj.entrySet().toArray()[j]).getKey().toString();
-//            LOG.debug("filePath " + filePath);
-//
-//            JsonArray coverageArray = coverageJsonObj.get(filePath).getAsJsonArray();
-//
-//            // for each line in the coverage array
-//            for (int i = 0; i < coverageArray.size(); i++) {
-//                Long line = null;
-//
-//                if (!coverageArray.get(i).isJsonNull()) {
-//                    line = coverageArray.get(i).getAsLong();
-//                }
-//
-//                Integer intLine = 0;
-//                int lineNumber = i + 1;
-//                if (line != null) {
-//                    intLine = line.intValue();
-//                    fileCoverage.setHits(lineNumber, intLine);
-//                }
-//            }
-//            LOG.info("FILE COVERAGE = " + fileCoverage.getCoveredLines());
-//            coveredFiles.put(filePath, fileCoverage);
-//        }
     }
 
     private CoverageReport readAndParseReportFile(File file) throws IOException {
