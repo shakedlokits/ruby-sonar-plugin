dc9a326713d065564fe99a7e2da1e7898fa570e7
Sergey Gernyak <sergeg1990@gmail.com>
2017-03-31 08:44:08 +0300

    Added comments for CoverageReportFileAnalyzer

--- src/main/java/com/godaddy/sonar/ruby/simplecovrcov/CoverageReportFileAnalyzer.java
+++ src/main/java/com/godaddy/sonar/ruby/simplecovrcov/CoverageReportFileAnalyzer.java
@@ -8,5 +8,12 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface CoverageReportFileAnalyzer extends BatchExtension {
+    /**
+     * Analyzes a given coverage report file
+     *
+     * @param file an instance of a coverage report file
+     * @return a map where a key is a file name and a value is a coverage information for this file
+     * @throws IOException when coverage report file could not be read
+     */
     Map<String, CoverageMeasuresBuilder> analyze(File file) throws IOException;
 }
