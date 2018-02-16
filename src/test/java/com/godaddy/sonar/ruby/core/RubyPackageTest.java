64d10bcfc41bc7cda191afeffc9a92ce3a22f1d0
Jared Wilkerson <jwilkers@redhat.com>
2018-02-16 09:17:55 -0500

    Fix RubyPackage and its tests for SonarQube 6.7
    - Update Qualifiers to use the updated identifier.

--- src/test/java/com/godaddy/sonar/ruby/core/RubyPackageTest.java
+++ src/test/java/com/godaddy/sonar/ruby/core/RubyPackageTest.java
@@ -64,7 +64,7 @@ public class RubyPackageTest {
 
     @Test
     public void testGetQualifier() {
-        assertEquals(Qualifiers.PACKAGE, rubyPackage.getQualifier());
+        assertEquals(Qualifiers.PROJECT, rubyPackage.getQualifier());
     }
 
 }
